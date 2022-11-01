312c0ad4f445959f9c26eea3d6f0935422194ecd
NestJS Haters Task Force <nestjs-haters-task-force@example.com>
2022-11-01 16:21:48 +0100

    work on oauth2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,20 +1,61 @@
 import { NestFactory } from '@nestjs/core';
-import { Controller, Get } from '@nestjs/common';
-import { Module } from '@nestjs/common';
+import { Controller, Get, Module, UseGuards, Res, Req, Injectable, Logger } from '@nestjs/common';
+import { AuthGuard, PassportStrategy, PassportModule } from '@nestjs/passport';
+import { Strategy as OAuth2Strategy } from 'passport-oauth2';
 
-@Controller()
+@Injectable()
+class MyStrategy extends PassportStrategy(OAuth2Strategy, 'oauth2') {
+	constructor() {
+		super({
+		    authorizationURL: 'https://api.intra.42.fr/oauth/authorize',
+		    tokenURL: 'https://api.intra.42.fr/oauth/token',
+		    clientID: '',
+		    clientSecret: '',
+		    callbackURL: 'http://localhost:3000/oauth/hello',
+		    passReqToCallback: true,
+		    scope: 'AUTH_SCOPE',
+		});
+	}
+
+	async validate(request: any, accessToken: string, refreshToken: string, profile, done: (err, user) => void) {
+		try {
+			const user = {accessToken};
+			console.log("hello");
+			done(null, user);
+		} catch (err) {
+			console.log("no");
+			done(err, false);
+		}
+	}
+}
+
+@Controller('oauth')
 class AppController {
 
-  @Get()
-  getHello(): string {
-    return "Hello World";
+  @Get('hello')
+  @UseGuards(AuthGuard('oauth2'))
+  getMessage(): string {
+	  return "Hello There";
+  }
+
+  @Get('login')
+  @UseGuards(AuthGuard('oauth2'))
+  async login() {
+	  Logger.log('login');
+	  return "Hello World";
+  }
+
+  @Get('get_token')
+  @UseGuards(AuthGuard('oauth2'))
+  async getToken(@Req() req, @Res() res: Response) {
+
   }
 }
 
 @Module({
-  imports: [],
+  imports: [PassportModule.register({defaultStrategy: 'oauth2', session: true})],
   controllers: [AppController],
-  providers: [],
+  providers: [MyStrategy],
 })
 class AppModule {}
 
