35618bcff3213add04cfd345d3f9c01835e28fea
NestJS Haters Task Force <nestjs-haters-task-force@example.com>
2022-11-01 14:52:11 +0100

    cleanup

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,22 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { Controller, Get } from '@nestjs/common';
+import { Module } from '@nestjs/common';
+
+@Controller()
+class AppController {
+
+  @Get()
+  getHello(): string {
+    return "Hello World";
+  }
+}
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [],
+})
+class AppModule {}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
