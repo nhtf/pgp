07d6dfd51d11051ad291312807c1db2210850a69
NestJS Haters Task Force <nestjs-haters-task-force@example.com>
2022-11-10 11:25:33 +0100

    oauth authentication

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,5 @@
 import { AppController } from './app.controller';
 import { Module } from '@nestjs/common';
-import { OAuth2Guard } from './auth/oauth.guard';
 import { WSConnection } from './wsconnection';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth/auth.controller';
@@ -11,6 +10,6 @@ import { AuthController } from './auth/auth.controller';
 		/*TypeOrmModule.forRoot({ type: 'postgres', username: 'postgres', password: 'postgres', host: '172.19.0.2' }),*/
 	],
 	controllers: [AppController, AuthController],
-	providers: [OAuth2Guard, WSConnection],
+	providers: [WSConnection],
 })
 export class AppModule { }
--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,20 +1,71 @@
-import { Controller, Get, UseGuards, Req, Res, Put } from '@nestjs/common';
+import { Controller, Get, UseGuards, Req, Res, Put, Post } from '@nestjs/common';
 import { Request, Response } from 'express';
-import { OAuth2Guard } from './oauth.guard';
 import { AuthorizationCode } from 'simple-oauth2';
+import * as session from 'express-session';
+import isAlphanumeric from 'validator/lib/isAlphanumeric';
+
+declare module 'express-session' {
+	export interface SessionData {
+		access_token: object;
+	}
+}
 
 @Controller('oauth')
 export class AuthController {
 
+	private client = new AuthorizationCode({
+		client: {
+			id: process.env.CLIENT_ID,
+			secret: process.env.CLIENT_SECRET,
+		},
+		auth: {
+			tokenHost: 'https://api.intra.42.fr',
+			authorizeHost: 'https://api.intra.42.fr',
+		}
+	});
+
 	@Get('login')
-	@UseGuards(OAuth2Guard)
-	async getHello() {
-		return 'well hello there';
+	async login(@Res() response: Response) {
+		const auth_uri = this.client.authorizeURL({
+			redirect_uri: 'http://localhost:3000/oauth/callback',
+			scope: 'public'
+		});
+		response.redirect(auth_uri);
+		return response.send();
 	}
 
 	@Get('callback')
-	@UseGuards(OAuth2Guard)
 	async get_token(@Req() request: Request, @Res() response: Response) {
-		return response.redirect('http://localhost:3000');
+		if (!request.query || !request.query.code) {
+			return response.redirect('http://localhost:3000/oauth/login');
+		}
+
+		const code = request.query.code;
+		if (!(typeof code === 'string'))
+			return response.status(400).json('bad request').send();
+
+		if (!isAlphanumeric(code))
+			return response.status(400).json('malformed code').send();
+
+		try {
+			const access_token = await this.client.getToken({
+				code: code,
+				redirect_uri: 'http://localhost:3000/oauth/callback',
+				scope: 'public'
+			});
+
+			request.session.regenerate((err) => {
+				if (err) {
+					response.status(503).json('failed to create a session');
+					return response.send();
+				}
+			});
+
+			request.session.access_token = access_token;
+			response.redirect('http://localhost:5173/');
+		} catch (_) {
+			response.status(403).json('forbidden');
+		}
+		return response.send();
 	}
 }
