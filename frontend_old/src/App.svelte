284501db9202b1d5a7f59b5d2ec58b65df6947b4
NestJS Haters Task Force <nestjs-haters-task-force@example.com>
2022-11-17 10:55:08 +0100

    remove old files

--- frontend_old/src/App.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import Pong from './Pong.svelte';
-	import Socket from './socket.svelte';
-	import Vr from './Vr.svelte';
-</script>
-<!-- <Pong /> -->
-<!-- <Socket /> -->
-<Vr />
-  
--- frontend_sveltekit/src/routes/pong/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-    import World from "./World/Worldthing.svelte";
-</script>
-
-<!-- <p>pong should be here</p> -->
-<World />
